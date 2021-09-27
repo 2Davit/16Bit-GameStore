676be7773c8904838bf16fb099a44ba14a7fde9c
Diosquezfran <80664353+Diosquezfran@users.noreply.github.com>
2021-09-26 23:06:31 -0300

    probando git

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
-      <span>A dársela con TypeScript!</span>
+      <span>A dársela con TypeScript! porbando branch</span>
     </div>
   );
 }
