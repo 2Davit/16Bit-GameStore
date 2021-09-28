9ef4e30823fb3d67fb84499d5c63394b804b9a12
Denis <82286878+DenisOrlando@users.noreply.github.com>
2021-09-27 21:22:13 -0300

    finalizada la base de datos

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-/* const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const morgan = require('morgan'); */
+const morgan = require('morgan');
 const routes = require('./routes/index.js');
 
 require('./db.js');
@@ -10,10 +10,10 @@ const server = express();
 
 server.name = 'API';
 
-/* server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
-server.use(morgan('dev')); */
+server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
