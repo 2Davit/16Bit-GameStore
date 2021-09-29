1f0ea9d5808ca991d192fbe00d41f59f84bf126d
chakkyy <cmramirez1996@gmail.com>
2021-09-28 23:06:08 -0300

    modalCart v  0.1.0

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as NavBar } from './NavBar/NavBar';
 export { default as Card } from './Card/Card';
 export { default as Paginate } from './Paginate/Paginate';
+export { default as CartSideBar } from './CartSideBar/CartSideBar';
 
