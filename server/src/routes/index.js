c9e95f801a780e2e80a3e98935727ee3b45e37c0
Denis <82286878+DenisOrlando@users.noreply.github.com>
2021-09-28 22:49:11 -0300

    finalizada ruta de búsqueda y todo

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -13,6 +13,64 @@ router.post("/platforms", async (req, res) => {
   res.json(await Platform.bulkCreate(req.body));
 });
 
+//Rutas de videogames.
+//Ruta de get de todos los videogames.
+router.get("/videogames", async (req, res) => {
+  try {
+    const getDbInfo = async () => {
+      return await Product.findAll({
+        include: [
+          {
+            model: Genre,
+            attributes: ["name_genre"],
+            trough: {
+              attributes: [],
+            },
+          },
+          {
+            model: Platform,
+            attributes: ["name_platform"],
+            trough: {
+              attributes: [],
+            },
+          },
+        ],
+      });
+    };
+    const dbInfoA = await getDbInfo();
+    const dbInfoB = await dbInfoA.map((el) => {
+      return {
+        id_product: el.dataValues.id_product,
+        name_product: el.dataValues.name_product,
+        price_product: el.dataValues.price_product,
+        description_product: el.dataValues.description_product,
+        thumbnail_product: el.dataValues.thumbnail_product,
+        in_stock: el.dataValues.in_stock,
+        on_sale: el.dataValues.on_sale,
+        release_year: el.dataValues.release_year,
+        name_genre: el.dataValues.genres?.map((el) => el.name_genre),
+        name_platform: el.dataValues.platforms?.map((el) => el.name_platform),
+      };
+    });
+    const { name } = req.query;
+    if (name) {
+      let gameName = await dbInfoB.filter((v) =>
+        v.name_product.toLowerCase().includes(name.toLowerCase())
+      );
+      if (gameName.length >= 1) return res.status(200).send(gameName);
+      res
+        .status(404)
+        .send("The videogame doesn't exist, please check the name");
+    } else {
+      res.status(200).send(dbInfoB);
+    }
+    //fin del try!!!!
+  } catch (error) {
+    console.log(error);
+    res.status(404).send(error);
+  }
+});
+
 //Ruta posteo de un post.
 router.post("/videogame", async (req, res) => {
   const {
