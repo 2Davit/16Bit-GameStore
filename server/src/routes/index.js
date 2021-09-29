4451c45c57d28cc0c813e662d1d8ee65779a9d6b
Alex4196 <alexgoumaz@hotmail.com>
2021-09-29 01:05:41 -0300

    rutas del back nuevas

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -13,6 +13,165 @@ router.post("/platforms", async (req, res) => {
   res.json(await Platform.bulkCreate(req.body));
 });
 
+//filtra por on_sale
+
+router.get('/videogamesOnsale', async (req,res) => {
+    try{
+const gamesOnSale = await Product.findAll(
+    {
+        where: {
+            on_sale: true
+        },
+        include: [
+            {
+              model: Genre,
+              attributes: ["name_genre"],
+              through: {
+                attributes: [],
+              },
+            },
+            {
+             model: Platform,
+             attributes: ["name_platform"],
+             through: {
+               attributes: [],
+             },
+           },
+          ],
+    }
+)
+res.status(200).send(gamesOnSale)
+
+    }catch(error){
+       res.status(404).send("error")
+    }
+})
+
+
+
+//filtra por genero
+router.get('/videogamesGenres', async (req, res) => {
+    try {
+     const getDbInfo = async () => {
+       return await Product.findAll({
+         include: [
+           {
+             model: Genre,
+             attributes: ["name_genre"],
+             through: {
+               attributes: [],
+             },
+           },
+           {
+            model: Platform,
+            attributes: ["name_platform"],
+            through: {
+              attributes: [],
+            },
+          },
+         ],
+       });
+     };
+     const dbInfoA = await getDbInfo();
+       const dbInfoB = await dbInfoA.map((el) => {
+         return {
+            name_platform: el.dataValues.platforms?.map((el) => el.name_platform),
+           id_product: el.dataValues.id_product,
+           name_product: el.dataValues.name_product,
+           price_product: el.dataValues.price_product,
+           description_product: el.dataValues.description_product,
+           thumbnail_product: el.dataValues.thumbnail_product,
+           in_stock: el.dataValues.in_stock,
+           on_sale: el.dataValues.on_sale,
+           release_year: el.dataValues.release_year,
+           name_genre: el.dataValues.genres?.map((el) => el.name_genre),
+           
+         };
+       });
+     
+         const {genre} = req.query;
+         if (genre) {
+            let gameGenre = await dbInfoB.filter((v) => {
+                for (let i = 0; i < v.name_genre.length; i++) {
+                  if (v.name_genre[i] === genre) return v.name_genre[i];
+                }
+              }
+              );
+           if (gameGenre.length >= 1) return res.status(200).send(gameGenre);
+           res
+             .status(404)
+             .send("No results match with the requested genre, please check the genre");
+         } else {
+           res.status(200).send(dbInfoB);
+          
+         }
+    } catch (error) {
+      res.status(404).send('Hubo un error: ' + error)
+    }
+   })
+
+//filtra por plataforma
+   router.get('/videogamesPlatform', async (req, res) => {
+    try {
+     const getDbInfo = async () => {
+       return await Product.findAll({
+         include: [
+           {
+             model: Genre,
+             attributes: ["name_genre"],
+             through: {
+               attributes: [],
+             },
+           },
+           {
+            model: Platform,
+            attributes: ["name_platform"],
+            through: {
+              attributes: [],
+            },
+          },
+         ],
+       });
+     };
+     const dbInfoA = await getDbInfo();
+       const dbInfoB = await dbInfoA.map((el) => {
+         return {
+            name_platform: el.dataValues.platforms?.map((el) => el.name_platform),
+           id_product: el.dataValues.id_product,
+           name_product: el.dataValues.name_product,
+           price_product: el.dataValues.price_product,
+           description_product: el.dataValues.description_product,
+           thumbnail_product: el.dataValues.thumbnail_product,
+           in_stock: el.dataValues.in_stock,
+           on_sale: el.dataValues.on_sale,
+           release_year: el.dataValues.release_year,
+           name_genre: el.dataValues.genres?.map((el) => el.name_genre),
+           
+         };
+       });
+     
+         const {platform} = req.query;
+         if (platform) {
+            let gamePlatform = await dbInfoB.filter((v) => {
+                for (let i = 0; i < v.name_platform.length; i++) {
+                  if (v.name_platform[i] === platform) return v.name_platform[i];
+                }
+              }
+              );
+           if (gamePlatform.length >= 1) return res.status(200).send(gamePlatform);
+           res
+             .status(404)
+             .send("No results match with the requested genre, please check the genre");
+         } else {
+           res.status(200).send(dbInfoB);
+          
+         }
+    } catch (error) {
+      res.status(404).send('Hubo un error: ' + error)
+    }
+   })
+
+
 //Rutas de videogames.
 //Ruta de get de todos los videogames.
 router.get("/videogames", async (req, res) => {
@@ -23,14 +182,14 @@ router.get("/videogames", async (req, res) => {
           {
             model: Genre,
             attributes: ["name_genre"],
-            trough: {
+            through: {
               attributes: [],
             },
           },
           {
             model: Platform,
             attributes: ["name_platform"],
-            trough: {
+            through: {
               attributes: [],
             },
           },
@@ -162,6 +321,32 @@ router.post("/videogames", async (req, res) => {
   });
   res.status(200).send("Product succesfully added");
 });
+
+router.get("/videogames/:id", async (req, res) => {
+    const {id} = req.params;
+  
+    try {
+      let product = await Product.findByPk(id, {
+        include: [
+          {
+            model: Platform,
+            attributes: ["name_platform"],
+            through: { attributes: [] },
+          },
+          {
+            model: Genre,
+            attributes: ["name_genre"],
+            through: { attributes: [] },
+          }
+        ]
+      })
+      res.status(200).send(product) 
+    } catch (error) {
+      res.status(404).send('not found')
+    }
+  
+  })
+
 
 //Ruta de posteo de user.
 router.post("/user", async (req, res) => {
