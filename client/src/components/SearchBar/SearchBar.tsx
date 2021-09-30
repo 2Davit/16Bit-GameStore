3f34f4fce3ccac67539f9b255035cecd2d9f5d33
chakkyy <cmramirez1996@gmail.com>
2021-09-30 12:56:09 -0300

    fix(searchbar): error minimo con props fixd

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,35 +1,30 @@
-import React, { useState, FC } from 'react';
-import { useDispatch } from 'react-redux';
-import { getNameProduct } from '../../redux/actions/products_action'
+import React, { useState, FC } from "react";
+import { useDispatch } from "react-redux";
+import { getNameProduct } from "../../redux/actions/products_action";
 
-interface Props {
-    currentPage(pageNum: number): void
-}
+const SearchBar = () => {
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
 
-const SearchBar: FC<Props> = ({currentPage}) => {
-    const [name, setName] = useState('')
-    const dispatch = useDispatch();
+  const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setName(e.currentTarget.value);
+  };
+  const handleSubmit = () => {
+    dispatch(getNameProduct(name));
+    setName("");
+  };
 
-    const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
-        setName(e.currentTarget.value)
-    }
-    const handleSubmit = () => {
-        dispatch(getNameProduct(name))
-        setName('')
-        currentPage(1)
-    }
-    
-    return (
-        <div>
-            <input 
-                type="text"
-                placeholder="find videogame..."
-                value={name}
-                onChange={(e) => handleInputChange(e)}
-            />
-            <button onClick={() => handleSubmit()} >Search</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="find videogame..."
+        value={name}
+        onChange={(e) => handleInputChange(e)}
+      />
+      <button onClick={() => handleSubmit()}>Search</button>
+    </div>
+  );
+};
 
 export default SearchBar;
