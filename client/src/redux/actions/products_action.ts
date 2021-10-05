f1ddb556ecaebb08a182be19519b5be7090b75bd
Denis <82286878+DenisOrlando@users.noreply.github.com>
2021-10-05 17:38:23 -0300

    Feat(filterByDB) filtrado según la plataforma o genero de la DB

--- client/src/redux/actions/products_action.ts
+++ client/src/redux/actions/products_action.ts
@@ -1,68 +1,78 @@
-
 import axios from "axios";
-import { Product, ProductCreate, CreateGenre, CreatePlatform } from "../../interfaces";
+import {
+  Product,
+  ProductCreate,
+  CreateGenre,
+  CreatePlatform,
+  Genre,
+  Platform,
+} from "../../interfaces";
 import {
   GET_ALL_PRODUCTS,
   GET_PRODUCT_DETAIL,
   GET_NAME_PRODUCT,
   DOUBLE_FILTER,
-  GET_PRODUCT_ON_SALE
+  GET_PRODUCT_ON_SALE,
+  GET_ALL_GENRES,
+  GET_ALL_PLATFORMS,
 } from "../types";
 
 import { Dispatch } from "redux";
 //Discutir el uso de try-catch en los actions, debido a que siempre va a estar funcional hasta este punto porque el back esta funcional y sin bugs, para reducir la cantidad de warnings innecesarios que tenemos en el codigo ya que quedan los catch como "codigo inaccesible"
-
 
 interface AllProducts {
   type: string;
   payload: Array<Product> | Product;
 }
 
+interface AllGenres {
+  type: string;
+  payload: Array<Genre>;
+}
 
-interface Detail { 
-    type: string;
+interface AllPlatforms {
+  type: string;
+  payload: Array<Platform>;
+}
+
+interface Detail {
+  type: string;
 }
 
 interface Name {
-    type: string;
-    payload: Product
-
+  type: string;
+  payload: Product;
 }
 
 export const getAllProducts = () => {
   return async (dispatch: Dispatch<AllProducts>): Promise<any> => {
-      const totalProducts = await axios.get("http://localhost:3001/videogames");
-      return dispatch({
-        type: GET_ALL_PRODUCTS,
-        payload: totalProducts.data,
-      });
-    };
-  } 
-
+    const totalProducts = await axios.get("http://localhost:3001/videogames");
+    return dispatch({
+      type: GET_ALL_PRODUCTS,
+      payload: totalProducts.data,
+    });
+  };
+};
 
 export const getProductDetail = (id: number) => {
-      return async (dispatch: Dispatch<Detail>): Promise<any> => {
-      var json = await axios.get(`http://localhost:3001/videogames/${id}`);
-      return dispatch({
-        type: GET_PRODUCT_DETAIL,
-        payload: json.data,
-      });
-    };
-  } 
+  return async (dispatch: Dispatch<Detail>): Promise<any> => {
+    var json = await axios.get(`http://localhost:3001/videogames/${id}`);
+    return dispatch({
+      type: GET_PRODUCT_DETAIL,
+      payload: json.data,
+    });
+  };
+};
 
+export const getNameProduct = (name: string) => {
+  return async (dispatch: Dispatch<Name>): Promise<any> => {
+    let json = await axios.get(`http://localhost:3001/videogames?name=${name}`);
 
-export const getNameProduct = (name: string) => {   
-      return async (dispatch: Dispatch<Name>): Promise<any> => {
-  
-        let json = await axios.get(
-          `http://localhost:3001/videogames?name=${name}`
-          );
-          
-        return dispatch({
-          type: GET_NAME_PRODUCT,
-          payload: json.data,
-        });
-      };   
+    return dispatch({
+      type: GET_NAME_PRODUCT,
+      payload: json.data,
+    });
+  };
 };
 
 export const doubleFilter = function (payload: any) {
@@ -72,36 +82,61 @@ export const doubleFilter = function (payload: any) {
   };
 };
 
-
 export const onSaleFilter = (place: string) => {
-    return async (dispatch: Dispatch<Detail>): Promise<any> => {
-      var json = await axios.get(`http://localhost:3001/videogames/Onsale`);
-      return dispatch({
-        type: GET_PRODUCT_ON_SALE,
-        payload: {
-          data: json.data,
-          place
-        }
-      });
-    };
-  }
+  return async (dispatch: Dispatch<Detail>): Promise<any> => {
+    var json = await axios.get(`http://localhost:3001/videogames/Onsale`);
+    return dispatch({
+      type: GET_PRODUCT_ON_SALE,
+      payload: {
+        data: json.data,
+        place,
+      },
+    });
+  };
+};
 
-
-export const createVideogame =(payload: ProductCreate) => {
-    return async function (dispatch: Dispatch<Name>) {
-        const data = await axios.post("http://localhost:3001/videogames/OneGame", payload);
-        return data;
-    }
-}
-export const createNewGenre =(payload: CreateGenre) => {
+export const createVideogame = (payload: ProductCreate) => {
   return async function (dispatch: Dispatch<Name>) {
-      const data = await axios.post("http://localhost:3001/genres/oneGenre", payload);
-      return data;
-  }
-}
-export const createNewPlatform =(payload: CreatePlatform) => {
+    const data = await axios.post(
+      "http://localhost:3001/videogames/OneGame",
+      payload
+    );
+    return data;
+  };
+};
+export const createNewGenre = (payload: CreateGenre) => {
   return async function (dispatch: Dispatch<Name>) {
-      const data = await axios.post("http://localhost:3001/platforms/onePlatform", payload);
-      return data;
-  }
-}
+    const data = await axios.post(
+      "http://localhost:3001/genres/oneGenre",
+      payload
+    );
+    return data;
+  };
+};
+export const createNewPlatform = (payload: CreatePlatform) => {
+  return async function (dispatch: Dispatch<Name>) {
+    const data = await axios.post(
+      "http://localhost:3001/platforms/onePlatform",
+      payload
+    );
+    return data;
+  };
+};
+export const getAllGenres = () => {
+  return async (dispatch: Dispatch<AllGenres>): Promise<any> => {
+    const json = await axios.get("http://localhost:3001/genres/");
+    return dispatch({
+      type: GET_ALL_GENRES,
+      payload: json.data,
+    });
+  };
+};
+export const getAllPlatforms = () => {
+  return async (dispatch: Dispatch<AllPlatforms>): Promise<any> => {
+    const json = await axios.get("http://localhost:3001/platforms/");
+    return dispatch({
+      type: GET_ALL_PLATFORMS,
+      payload: json.data,
+    });
+  };
+};
