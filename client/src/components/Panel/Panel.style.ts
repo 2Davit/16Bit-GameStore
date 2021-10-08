689bb6ee25ed306c3ea4656a7549cf27bc1dc185
Armando Perez <armandlord98@gmail.com>
2021-10-08 12:36:54 -0500

    terminao el AdminProduct

--- client/src/components/Panel/Panel.style.ts
+++ client/src/components/Panel/Panel.style.ts
@@ -3,13 +3,30 @@ import { AiFillHome } from 'react-icons/ai'
 import { FaMoneyBillWave } from 'react-icons/fa'
 import { BiJoystickAlt } from 'react-icons/bi'
 import { HiUsers } from 'react-icons/hi'
+import { Link } from 'react-router-dom'
 
 export const ContainerPanel = styled.div`
     width: 100%;
     height: 100%;
     display: flex;
     flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 0;
     background: #222831;
+`
+export const LogoLink = styled(Link)`
+    width: 100%;
+    height: 5%;
+    background: #000000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+export const LogoAdmin = styled.img`
+    width: 65%;
+    height: 100%;
+    object-fit: cover;
 `
 export const  ContainerImageUser = styled.div`
     width: 100%;
@@ -17,15 +34,16 @@ export const  ContainerImageUser = styled.div`
     display: flex;
     align-items: center;
 `
+export const  NameImage = styled.h5`
+    font-size: .8rem;
+    margin: 10px 0 0 0;
+`
 export const  Image = styled.img`
     width: 50px;
     height: 50px;
     border-radius: 50%;
     object-fit: cover;
-    margin: 0 30px;
-`
-export const  NameImage = styled.h5`
-    font-size: .8rem;
+    margin: 0 10px 0 20px ;
 `
 export const  BtnAdminPanel = styled.button`
     height: 50px;
@@ -37,6 +55,7 @@ export const  BtnAdminPanel = styled.button`
     display: flex;
     align-items: center;
     font-size: 1.2rem;
+    padding: 10px;
     &:hover{
         border-left: 3px solid #B55400;
         border-right: 3px solid transparent;
