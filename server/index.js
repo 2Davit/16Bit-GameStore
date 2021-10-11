99cf17497b01270ae43e717d79be3afeeae12e49
Denis <82286878+DenisOrlando@users.noreply.github.com>
2021-10-11 19:06:55 -0300

    prueba deploy 2

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen({port : process.env.PORT || 3001}, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
