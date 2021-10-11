da79ee4e196d650eb96707881b6d38be3ef0d47e
Carlos Ramirez <cmramirez1996@gmail.com>
2021-10-11 15:46:13 -0300

    fix(css): sugerencias searchbar

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Store } from "../../redux/reducer/";
-import {
-  getNameProduct,
-} from "../../redux/actions/products_action";
+import { getNameProduct } from "../../redux/actions/products_action";
 
 import { FormSearchBar, SearchResults } from "./StyledSearchBar";
 import loupe from "../../assets/img/svg/loupe.svg";
@@ -28,8 +26,10 @@ const SearchBar = ({ setPage }: any) => {
     dispatch(getNameProduct(inputText));
   };
   const handleChange = () => {
-    let handle = searchProducts.filter((index: any) => index.name_product.toLowerCase().includes(inputText.toLowerCase()));
-    setAutocomplete(handle.slice(0, 4))
+    let handle = searchProducts.filter((index: any) =>
+      index.name_product.toLowerCase().includes(inputText.toLowerCase())
+    );
+    setAutocomplete(handle.slice(0, 4));
   };
 
   return (
@@ -42,23 +42,23 @@ const SearchBar = ({ setPage }: any) => {
           value={inputText}
           onBlur={() => {
             setTimeout(() => {
-              setAutocomplete([])
-            },200)
+              setAutocomplete([]);
+            }, 200);
           }}
         />
-      <SearchResults>
-        {autoComplete.length !== 0 ? autoComplete.map((product: any) => {
-          return (
-            
-              <SearchCard
-                game={product}
-                key={product.id_product}
-                id={product.id_product}
-              />
-           
-          );
-        }) : ""}
-         </SearchResults>
+        <SearchResults style={{ display: inputText ? "block" : "none" }}>
+          {autoComplete.length !== 0
+            ? autoComplete.map((product: any) => {
+                return (
+                  <SearchCard
+                    game={product}
+                    key={product.id_product}
+                    id={product.id_product}
+                  />
+                );
+              })
+            : ""}
+        </SearchResults>
         <button type="submit">
           <img src={loupe} alt="" />
         </button>
