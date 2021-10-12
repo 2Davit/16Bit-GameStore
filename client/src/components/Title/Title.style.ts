77c014d451bbcc70fc57c1d943195b3a2a715531
Carlos Ramirez <cmramirez1996@gmail.com>
2021-10-12 12:37:40 -0300

    fix(css): titulo matchea colores

--- client/src/components/Title/Title.style.ts
+++ client/src/components/Title/Title.style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 
 export const StyledTitle = styled.div`
-background: url('https://www.ppt-backgrounds.net/thumbs/pixel-art-by-wupto-on-deviantart-design-keynote.png');
+background: url('https://w0.peakpx.com/wallpaper/847/514/HD-wallpaper-artistic-pixel-art-moon.jpg');
 background-position: top center;
 background-size: cover;
 display:flex;
@@ -11,12 +11,12 @@ border-radius: 999px;
 letter-spacing: 3px;
 font-weight: bold;
 margin:0 3em;
-padding: 1em;
+padding: 1.1em;
 &:hover{
-      color: #108d;
+      color: var(--clr-primary);
     }
 
     .gamestore{
-color: #108d;
+color: var(--clr-primary);
     }
 `
