5f2797619190e7ae9b13c63f4a708ab53ea89b47
Diosquezfran <80664353+Diosquezfran@users.noreply.github.com>
2021-10-12 20:05:28 -0300

    nodemailer

--- server/src/controllers/User.js
+++ server/src/controllers/User.js
@@ -2,6 +2,7 @@ const { User } = require("../db");
 const axios = require("axios");
 const jwt = require("jsonwebtoken");
 const { SECRET } = process.env;
+const nodemailer = require("nodemailer");
 
 async function postUser(req, res) {
   const {
@@ -57,7 +58,41 @@ async function getUsers(req, res) {
   }
 }
 
+async function sendUserMail (req, res){
+
+  let {email} = req.body;
+
+  let transporter = nodemailer.createTransport({
+    host: 'imap.ethereal.email',
+    post: 993,
+    secure: false,
+    auth: {
+        user: 'guido.gambin',
+        pass: 'sagbdfnDBBpnUtzdmZ'
+    }
+})
+//diosquezfran@gmail.com
+
+  let mailOptions = {
+    from: "Remitente",
+    to: email,
+    subject: "Gracias por su compra",
+    text: "Gracias por confiar en 16-bits"
+  } 
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if(error){
+      res.status(500).send(error.message)
+    }else {
+      console.log('email enviado');
+      res.status(200).send(req.body)
+    }
+  })
+
+}
+
 module.exports = {
   postUser,
-  getUsers
+  getUsers,
+  sendUserMail
 };
--- server/src/routes/User.js
+++ server/src/routes/User.js
@@ -2,9 +2,10 @@ const { Router } = require("express");
 
 const router = Router();
 
-const { postUser, getUsers } = require("../controllers/User");
+const { postUser, getUsers, sendUserMail } = require("../controllers/User");
 
 router.post("/", postUser);
+router.post("/mail", sendUserMail);
 router.get("/", getUsers);
 
 module.exports = router;
