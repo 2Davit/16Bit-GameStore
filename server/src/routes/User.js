fc4519b1054d1f8140282c7f321e507d920e1f65
Carlos Ramirez <cmramirez1996@gmail.com>
2021-10-13 19:01:35 -0300

    feat(user): borra y editar usuario

--- server/src/routes/User.js
+++ server/src/routes/User.js
@@ -2,11 +2,17 @@ const { Router } = require("express");
 
 const router = Router();
 
-const { postUser, getUsers, deleteOneUser, banOneUser } = require("../controllers/User");
+const {
+  postUser,
+  getUsers,
+  deleteOneUser,
+  banOneUser,
+  updateOneUser,
+} = require("../controllers/User");
 
 router.post("/", postUser);
 router.get("/", getUsers);
 router.delete("/:id", deleteOneUser);
 router.put("/:id/:status", banOneUser);
-
+router.put("/updateuser", updateOneUser);
 module.exports = router;
