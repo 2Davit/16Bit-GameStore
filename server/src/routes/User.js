7151d2bcac2ab95440b56f048bd146cf70264375
Armando Perez <armandlord98@gmail.com>
2021-10-12 20:34:27 -0500

    Banear usuarios funciona, back y front

--- server/src/routes/User.js
+++ server/src/routes/User.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 
 const router = Router();
 
-const { postUser, getUsers } = require("../controllers/User");
+const { postUser, getUsers, deleteOneUser, banOneUser } = require("../controllers/User");
 
 router.post("/", postUser);
 router.get("/", getUsers);
+router.delete("/:id", deleteOneUser);
+router.put("/:id/:status", banOneUser);
 
 module.exports = router;
