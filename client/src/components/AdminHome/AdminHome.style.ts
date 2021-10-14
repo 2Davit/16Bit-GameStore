7f9f4d94153dd0dc25715caacbe365ed55deb045
Armando Perez <armandlord98@gmail.com>
2021-10-14 17:41:51 -0500

    admin home con grafica y dos tablas

--- client/src/components/AdminHome/AdminHome.style.ts
+++ client/src/components/AdminHome/AdminHome.style.ts
@@ -8,16 +8,71 @@ export const ContainerAdminHome = styled.div`
 export const ContainerStadistic = styled.div`
     height: 95%;
     width: 100%;
-    /* display: flex;
+    display: flex;
+`
+export const Vertical = styled.div`
+    
+    width: 50%;
+    height: 100%;
+    display: flex;
     flex-direction: column;
-    align-items: center;
-    overflow: scroll; */
+`
+export const Vertical2 = styled.div`
+   
+    width: 50%;
+    height: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    /* align-items: center; */
+    margin-top: 3rem;
+`
+export const Horizontal = styled.div`
+    
+    width: 100%;
+    height: 50%;
+    display: flex;
+    justify-content: center;
+    margin-top: 3rem;
+    /* align-items: center; */
 `
 
 export const GraLine = styled.div`
-    width: 800px;
-    height: 400px;
-      
+    width: 600px;
+    height: 300px;
+    background: #e1e1e1;
     border: 2px solid rgba(255, 255, 255, 0.8);
     border-radius: 2.5rem;
+`
+
+export const TableInfo = styled.div`
+    background: #222831;
+    height: 80%;
+    width: 45%;
+    border-radius: 2.5rem;
+    border: 2px solid rgb(0, 0, 0);
+`
+export const H2 = styled.h2`
+   padding: 1.5em; 
+   font-size: 1.2rem;
+   background: #000000;
+   width: 100%;
+   border-top-left-radius: 2.5rem;
+   border-top-right-radius: 2.5rem;
+   text-align: center;
+`
+export const InfoDiv = styled.div`
+  border-bottom: 2px solid #393E46;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+  height: 40px;
+
+  h4{
+    font-size: 1.1;
+  }
+
+  h5{
+
+  }
 `
--- client/src/components/AdminHome/AdminHome.tsx
+++ client/src/components/AdminHome/AdminHome.tsx
@@ -1,81 +1,116 @@
-
 import { FC, useState } from "react";
 import { ContainerNav } from "../ProductContent/ProductContent.style";
-import { ContainerAdminHome, ContainerStadistic, GraLine } from "./AdminHome.style";
-import { VictoryLine, VictoryChart, VictoryTheme, Curve, VictoryArea, VictoryBar } from "victory";
+import {
+  ContainerAdminHome,
+  ContainerStadistic,
+  GraLine,
+  Vertical,
+  Vertical2,
+  Horizontal,
+  TableInfo,
+  H2,
+  InfoDiv
+} from "./AdminHome.style";
+import {
+  VictoryLine,
+  VictoryChart,
+  VictoryTheme,
+  Curve,
+  VictoryArea,
+  VictoryBar,
+} from "victory";
 import { Order, User, Product } from "../../interfaces";
-
 
 interface Props {
   totalOrders: Array<Order>;
   totalUsers: Array<User>;
-  totalProducts: Array<Product>
+  totalProducts: Array<Product>;
 }
 
-
 const AdminHome: FC<Props> = ({ totalOrders, totalUsers, totalProducts }) => {
+  const listUsers = totalUsers.filter(
+    (user) => user.id_user > totalUsers.length - 10
+  );
 
-  const listUsers = totalUsers.filter(user => user.id_user > (totalUsers.length - 10))
+  const listStock = totalProducts.filter((product) => product.in_stock <= 5);
 
-  const listStock = totalProducts.filter(product => product.in_stock <= 5);
-
-  // const lengthMonth = totalOrders.filter(el => el.date.slice(8, 10) === '14') funciona para día 
-  const lengthMonth = totalOrders.filter(el => el.date.slice(5, 7) === '10')
-  const monthAmount = lengthMonth.map(el => el.amount)
-  const total = monthAmount?.reduce((a, b) => a + b, 0)
-  console.log(total)
+  // const lengthMonth = totalOrders.filter(el => el.date.slice(8, 10) === '14') funciona para día
+  const lengthMonth = totalOrders.filter((el) => el.date.slice(5, 7) === "10");
+  const monthAmount = lengthMonth.map((el) => el.amount);
+  const total = monthAmount?.reduce((a, b) => a + b, 0);
+  console.log("stock", listStock);
 
   return (
     <ContainerAdminHome>
       <ContainerNav>
-        <h3>Armando EL INCREDULO SUPREMO</h3>
+        
       </ContainerNav>
       <ContainerStadistic>
-        <GraLine >
-          <VictoryChart theme={VictoryTheme.grayscale} height={400} width={800}>
-            <VictoryBar
-              // interpolation="natural"
-              style={{
-                data: { stroke: "#000" },
-                parent: { border: "1px solid #000000" },
-              }}
-              data={[
-                { x: "Jan", y: 0, label: `` },
-                { x: "Feb", y: 20, label: `$3000` },
-                { x: "Mar", y: 18, label: `$2200` },
-                { x: "Apr", y: 25, label: `$4000` },
-                { x: "May", y: 22, label: `$3100` },
-                { x: "Jun", y: 16, label: `$2000` },
-                { x: "Jul", y: 17, label: `$2200` },
-                { x: "Aug", y: 25, label: `$3900` },
-                { x: "Sep", y: 20, label: `$3000` },
-                { x: "Oct", y: 22, label: `$${total}` },
-                { x: "Nov", y: 0 },
-                { x: "Dec", y: 0 },
-              ]}
-            />
-          </VictoryChart>
-        </GraLine>
-        <div style={{ background: "blue", width: "20%" }}>
-          Latest Registered Users
-          {listUsers ? listUsers.map((user) => (
-            <div style={{ color: "black" }}>
-              <h5>{user.nickname}</h5>
-              <h5>{user.created.slice(0, 10)}</h5>
-            </div>
-          )) : 'nada'}
-        </div>
+        <Vertical>
+          <Horizontal>
+            <GraLine>
+              <VictoryChart
+                theme={VictoryTheme.grayscale}
+                height={300}
+                width={700}
+              >
+                <VictoryBar
+                  style={{
+                    data: { fill: "#B55400", width: 25 },
+                    parent: { border: "1px solid #ffffff" },
+                  }}
+                  data={[
+                    { x: "Jan", y: 0, label: `` },
+                    { x: "Feb", y: 20, label: `$3000` },
+                    { x: "Mar", y: 18, label: `$2200` },
+                    { x: "Apr", y: 25, label: `$4000` },
+                    { x: "May", y: 22, label: `$3100` },
+                    { x: "Jun", y: 16, label: `$2000` },
+                    { x: "Jul", y: 17, label: `$2200` },
+                    { x: "Aug", y: 25, label: `$3900` },
+                    { x: "Sep", y: 20, label: `$3000` },
+                    { x: "Oct", y: monthAmount.length, label: `$${total}` },
+                    { x: "Nov", y: 0 },
+                    { x: "Dec", y: 0 },
+                  ]}
+                />
+              </VictoryChart>
+            </GraLine>
+          </Horizontal>
+          <Horizontal>
+            <h2>nada</h2>
+        
+          </Horizontal>
+        </Vertical>
+        <Vertical2>
+          <TableInfo >
+            <H2>Latest Registered Users</H2>
+            {listUsers.length !== 0 ? (
+              listUsers.map((user) => (
+                <InfoDiv>
+                  <h4>{user.nickname}</h4>
+                  <h5>{user.created.slice(0, 10)}</h5>
+                </InfoDiv>
+              ))
+            ) : (
+              <h1 style={{ color: "red" }}>Sin usuarios</h1>
+            )}
+          </TableInfo>
 
-        <div style={{ background: "blue", width: "20%" }}>
-        Nearly out of stock products
-          {listStock ? listStock.slice(0, 10).map(product => (
-            <div style={{ color: "black" }}>
-              <h5>{product.name_product}</h5>
-              <h5>{product.in_stock}</h5>
-            </div>
-          )) : 'nada'}
-        </div>
-
+          <TableInfo >
+            <H2>Nearly out of stock products</H2>
+            {listStock.length !== 0 ? (
+              listStock.slice(0, 10).map((product) => (
+                <InfoDiv>
+                  <h4>{product.name_product}</h4>
+                  <h5>{product.in_stock}</h5>
+                </InfoDiv>
+              ))
+            ) : (
+              <h1 style={{ color: "red" }}>Sin products</h1>
+            )}
+          </TableInfo>
+        </Vertical2>
       </ContainerStadistic>
     </ContainerAdminHome>
   );
