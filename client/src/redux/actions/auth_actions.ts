805fd96573e74c730821dce98fce976a526e492c
2Davit <davidalicek@gmail.com>
2021-10-16 14:55:39 -0300

    (fix): getRole no funcionaba, (feat): rutas /admin, /createProduct y /createGenre del front requieren rol de admin

--- client/src/redux/actions/auth_actions.ts
+++ client/src/redux/actions/auth_actions.ts
@@ -2,9 +2,8 @@ import axios from "axios";
 import { User } from "../../interfaces";
 import { LOGIN, USER_ROLE } from "../types";
 import { Dispatch } from "react";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 import { Redirect, useHistory } from "react-router";
-
 
 interface loginProps {
   type: string;
@@ -14,28 +13,36 @@ interface loginProps {
 export const login = (values: any) => {
   return async (dispatch: Dispatch<loginProps>): Promise<any> => {
     try {
-      const userData = await axios.post(
-        "/auth/login",
-        values
-      );
-      
+      const userData = await axios.post("/auth/login", values);
+
       localStorage.setItem("userData", JSON.stringify(userData.data));
       const localCart = JSON.parse(localStorage.getItem("cart")!);
       if (!localCart?.length && userData.data.data.cart_orders.length) {
-        localStorage.setItem("cart", JSON.stringify(userData.data.data.cart_orders));
-      }
-      else if (localCart && userData.data.data.cart_orders.length) {
+        localStorage.setItem(
+          "cart",
+          JSON.stringify(userData.data.data.cart_orders)
+        );
+      } else if (localCart && userData.data.data.cart_orders.length) {
         for (let i = 0; i < localCart.length; i++) {
-              for (let j = 0; j < userData.data.data.cart_orders.length; j++) {
-                  if (userData.data.data.cart_orders[j].id_product === localCart[i].id_product) {
-                        localCart[i].quantity += userData.data.data.cart_orders[j].quantity;
-                  }
-              }
+          for (let j = 0; j < userData.data.data.cart_orders.length; j++) {
+            if (
+              userData.data.data.cart_orders[j].id_product ===
+              localCart[i].id_product
+            ) {
+              localCart[i].quantity +=
+                userData.data.data.cart_orders[j].quantity;
+            }
+          }
         }
         var lastArray = [...localCart];
         for (let i = 0; i < userData.data.data.cart_orders.length; i++) {
-          if (!localCart.find((a: any) => a.id_product === userData.data.data.cart_orders[i].id_product)) {
-              lastArray.push(userData.data.data.cart_orders[i])
+          if (
+            !localCart.find(
+              (a: any) =>
+                a.id_product === userData.data.data.cart_orders[i].id_product
+            )
+          ) {
+            lastArray.push(userData.data.data.cart_orders[i]);
           }
         }
         localStorage.setItem("cart", JSON.stringify(lastArray));
@@ -46,18 +53,18 @@ export const login = (values: any) => {
       });
     } catch (err) {
       Swal.fire({
-        title: 'Error!',
-        text: 'Invalid username or password',
-        icon: 'error',
-        confirmButtonText: 'Ups!'
-      })
+        title: "Error!",
+        text: "Invalid username or password",
+        icon: "error",
+        confirmButtonText: "Ups!",
+      });
+    }
   };
 };
-}
 export const getRole = () => {
   return async (dispatch: Dispatch<any>): Promise<any> => {
     const userData = JSON.parse(localStorage.getItem("userData") as string);
-    const token = userData?.token;
+    const token = userData?.data.token;
     try {
       const role = await axios.get("/auth/getRole", {
         headers: { "x-access-token": token },
@@ -68,7 +75,6 @@ export const getRole = () => {
       });
     } catch (err) {
       console.log(err);
-      
     }
   };
 };
