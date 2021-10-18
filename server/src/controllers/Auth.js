8330a1d27c3525691808e00ab150e48d5ffaedd4
2Davit <davidalicek@gmail.com>
2021-10-18 18:48:54 -0300

    Google login with Auth0

--- server/src/controllers/Auth.js
+++ server/src/controllers/Auth.js
@@ -49,6 +49,96 @@ const signUp = async (req, res) => {
   return res.status(200).send("User successfully registered!");
 };
 
+const signInGoogle = async (req, res) => {
+  const { username, name, lastname, email, picture } = req.body;
+
+  const user = await User.findOrCreate({
+    where: {
+      email_user: email.toLowerCase(),
+    },
+    defaults: {
+      nickname_user: username.toLowerCase(),
+      email_user: email.toLowerCase(),
+      name_user: capitalize(name),
+      lastname_user: capitalize(lastname),
+      avatar_user: picture,
+      loggedWithGoogle: true,
+    },
+    include: [
+      {
+        model: Order,
+        where: { status_order: "cart" },
+        required: false,
+      },
+    ],
+  });
+
+  if (user[0] && user[0].is_active === false) {
+    return res.status(403).send({ message: "Suspended account" });
+  }
+
+  if (user[0] && user[0].orders.length) {
+    user[0].orders.sort((a, b) => {
+      if (a.dataValues.id_order > b.dataValues.id_order) {
+        return 1;
+      } else if (b.dataValues.id_order > a.dataValues.id_order) {
+        return -1;
+      } else return 0;
+    });
+
+    const lastCartOrder = await Order.findOne({
+      where: {
+        id_order: user[0].orders[user[0].orders.length - 1].dataValues.id_order,
+      },
+      include: [
+        {
+          model: OrderProduct,
+        },
+      ],
+    });
+
+    const cleanOrder = lastCartOrder.orderProducts.map((pro) => pro.dataValues);
+
+    const mapClean = cleanOrder.map((p) => ({
+      id_product: p.productIdProduct,
+      quantity: p.quantity_orderProduct,
+    }));
+
+    const products = mapClean.map(
+      async (p) => await Product.findByPk(p.id_product)
+    );
+
+    const finalProducts = await Promise.all(products);
+    var final = finalProducts.map((e) => e.dataValues);
+
+    for (let i = 0; i < final.length; i++) {
+      mapClean.forEach((e) => {
+        if (final[i].id_product === e.id_product) {
+          final[i].quantity = e.quantity;
+        }
+      });
+    }
+  }
+
+  const token = jwt.sign({ id: user.id_user }, SECRET, {
+    expiresIn: 86400, // 24 hours
+  });
+
+  res.status(200).send({
+    data: {
+      token: token,
+      username: capitalize(user[0].nickname_user),
+      email: user[0].email_user,
+      name: user[0].name_user,
+      lastname: user[0].lastname_user,
+      address: user[0].address_user,
+      cart_orders: final ? final : [],
+      loggedWithGoogle: user[0].loggedWithGoogle,
+    },
+    id: user[0].id_user,
+  });
+};
+
 const logIn = async (req, res) => {
   const { username, password } = req.body;
 
@@ -153,4 +243,5 @@ module.exports = {
   logIn,
   signUp,
   getRole,
+  signInGoogle,
 };
