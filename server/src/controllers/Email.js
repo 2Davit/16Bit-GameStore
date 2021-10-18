e66371eed0709b39f46c0b5ee7c59dd14fcb4919
Guido Gambini <guidogambini@hotmail.com>
2021-10-18 11:01:35 -0300

    advances

--- server/src/controllers/Email.js
+++ server/src/controllers/Email.js
@@ -1,101 +1,47 @@
 const Sequelize = require("sequelize");
-const { Product, User, favorites } = require("../db.js");
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
+const { window } = require("./Helper");
+
 
 async function sendUserMail(req, res) {
+
+  
   const { email, username, action, info } = req.body;
   const { cart, total } = info;
-  let message;
 
-  switch (action) {
-    case "purchase":
-      message =
-        "We really appreciate your last visit. Hope you are enjoying your new videogame!";
-      break;
+  
 
-    case "signup":
-      message =
-        "You are welcome to our 16Bit community. Get your cart and start shopping!";
-      break;
+  if (action === 'purchase') {
 
-    default:
-      message;
+    let aux = ``;
+
+    cart.forEach(product => {
+
+        aux += `
+        
+        <div style="background-color: #9b5df7; display: flex; flex-direction: column; align-items: center; border-radius: 16px; margin: 1.5rem 0rem ;">
+        <img
+          src=${product.image_product[0]}
+          alt=${product.name_product}
+          style="width: 10rem; height: 10rem; border-top-left-radius: 16px; border-top-right-radius: 16px"
+        />
+        <div style="display: flex; flex-direction: column; align-items: flex-start;">
+          <h5 style="margin: 0; padding-top: 5px;">${product.name_product}</h5>
+          <h5 style="margin: 0; padding-top: 5px" >Price: ${product.price_product}</h5>
+          <h5 style="margin: 0; margin-bottom: 5px; padding-top: 5px" >Quantity: ${product.quantity}</h5>
+        </div>
+      </div>
+        
+        `
+
+        console.log(window.document.getElementById("container"));
+
+    })
+
   }
 
-  const html = `<!DOCTYPE html>
-  <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta name="x-apple-disable-message-reformatting">
-    <title></title>
-    <!--[if mso]>
-    <noscript>
-      <xml>
-        <o:OfficeDocumentSettings>
-          <o:PixelsPerInch>96</o:PixelsPerInch>
-        </o:OfficeDocumentSettings>
-      </xml>
-    </noscript>
-    <![endif]-->
-    <style>
-      table, td, div, h1, p {font-family: Arial, sans-serif;}
-    </style>
-  </head>
-  <body style="margin:0;padding:0;">
-    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#000;">
-      <tr>
-        <td align="center" style="padding:0;">
-          <table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
-            <tr>
-              <td align="center" style="padding:40px 0 30px 0;background:#300643;">
-                <img src="https://res.cloudinary.com/druj3xeao/image/upload/v1634145513/samples/16bit_p3aiqt.jpg" alt="" width="300" style="height:auto;display:block;" />
-              </td>
-            </tr>
-            <tr>
-              <td style="padding:36px 30px 42px 30px;">
-                <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
-                  <tr>
-                    <td style="padding:0 0 36px 0;color:#153643;">
-                      <h1 style="font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif;">Hello, ${username}! ${message}</h1>
-                      <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">The total amount of your order is: $${total}</p>
-                      <p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a target="_blank" href="https://16-bit-game-store.vercel.app/home" style="color:#ee4c50;text-decoration:underline;">16-Bit</a></p>
-                    </td>
-                </table>
-              </td>
-            </tr>
-            <tr>
-              <td style="padding:30px;background:#ee4c50;">
-                <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family:Arial,sans-serif;">
-                  <tr>
-                    <td style="padding:0;width:50%;" align="left">
-                      <p style="margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;">
-                        &reg; Someone, Somewhere 2021<br/><a href="http://www.example.com" style="color:#ffffff;text-decoration:underline;">Unsubscribe</a>
-                      </p>
-                    </td>
-                    <td style="padding:0;width:50%;" align="right">
-                      <table role="presentation" style="border-collapse:collapse;border:0;border-spacing:0;">
-                        <tr>
-                          <td style="padding:0 0 0 10px;width:38px;">
-                            <a href="http://www.twitter.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/tw_1.png" alt="Twitter" width="38" style="height:auto;display:block;border:0;" /></a>
-                          </td>
-                          <td style="padding:0 0 0 10px;width:38px;">
-                            <a href="http://www.facebook.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/fb_1.png" alt="Facebook" width="38" style="height:auto;display:block;border:0;" /></a>
-                          </td>
-                        </tr>
-                      </table>
-                    </td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-          </table>
-        </td>
-      </tr>
-    </table>
-  </body>
-  </html>`;
+  
 
   const CLIENT_ID =
     "629164237375-nd9vo40e7m7p82lr4s7bgecqebbn7i6v.apps.googleusercontent.com";
@@ -133,7 +79,7 @@ async function sendUserMail(req, res) {
       from: "16Bit-GameStore",
       to: email,
       subject: action === 'purchase' ? "The summary of your purchase 🎉" : "Welcome to 16Bit 🚀",
-      html: html,
+      html: window
     };
 
     const result = await transporter.sendMail(mailOptions);
