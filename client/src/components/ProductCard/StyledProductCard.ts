1bf692442b8c577f7a345198999b6de14779495c
Carlos Ramirez <cmramirez1996@gmail.com>
2021-10-20 20:56:14 -0300

    addtofavsCard

--- client/src/components/ProductCard/StyledProductCard.ts
+++ client/src/components/ProductCard/StyledProductCard.ts
@@ -2,38 +2,61 @@ import styled from "styled-components";
 
 export const StyledProductCard = styled.article`
   font-size: 0.8em;
-  /* border: 3px solid ${(p) => p.theme.bgBorder}; */
   width: 275px;
   border-radius: 10px;
   overflow: hidden;
-  /* box-shadow: 5px 5px 0px ${(p) => p.theme.bgBorder}; */
+  box-shadow: 5px 5px 0px ${(p) => p.theme.bgBorder};
   position: relative;
-   transition: all 0.35s ease-in-out; 
+  transition: all 0.35s ease-in-out;
   height: 400px;
-  /* margin-right: 4.5em; */
-  /* margin-top: 2em; */
-  font-size: ${p => p.theme.fontSize};
+  font-size: ${(p) => p.theme.fontSize};
+ 
+    .btn-fav {
+    border: none;
+    position: absolute;
+    right: 0;
+    margin-top: 0.5em;
+    margin-right: 0.5em;
+    background:white;
+    padding: 0.5em 0em;
+    border-radius: 999px;
+    animation: attention 2s ease infinite;
+    z-index: 9999;
 
-   &:hover {
+    svg {
+      fill: #FF7B7B;
+      height: 20px;
+      transition: all 0.2s ease-in-out;
+    }
+    &:hover {
+      svg {
+        fill: #F02E2E;
+      }
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+ 
+
+  &:hover {
     transform: scale(1.05);
-    box-shadow: 4px 5px 13px 5px rgba(0,0,0,0.36);
-    -webkit-box-shadow: 4px 5px 13px 5px rgba(0,0,0,0.36);
-    -moz-box-shadow: 4px 5px 13px 5px rgba(0,0,0,0.36);
-    
+    box-shadow: 4px 5px 13px 5px rgba(0, 0, 0, 0.36);
+    -webkit-box-shadow: 4px 5px 13px 5px rgba(0, 0, 0, 0.36);
+    -moz-box-shadow: 4px 5px 13px 5px rgba(0, 0, 0, 0.36);
 
     .card__content {
-    padding: 0.5em 1em;
-    height: 40%;
-    position: absolute;
-    width:100%;
-    bottom: 0;
-    display: flex;
-    flex-direction: column;
-    background: rgba(40, 40, 40, 0.70);
-      
-  }
-  
-  /* button {
+      padding: 0.5em 1em;
+      height: 40%;
+      position: absolute;
+      width: 100%;
+      bottom: 0;
+      display: flex;
+      flex-direction: column;
+      background: rgba(40, 40, 40, 0.7);
+    }
+
+    /* button {
     bottom: 0;
     font-size: 0.9em;
     margin-top: auto;
@@ -44,7 +67,7 @@ export const StyledProductCard = styled.article`
       outline: none;
     }
   } */
-  } 
+  }
 
   .card__link {
     color: transparent;
@@ -58,11 +81,10 @@ export const StyledProductCard = styled.article`
   .card__imgContainer {
     width: 100%;
     height: 100%;
-    
   }
 
   .card__img {
-    position:relative;
+    position: relative;
     width: 100%;
     height: 100%;
     object-position: center center;
@@ -77,19 +99,18 @@ export const StyledProductCard = styled.article`
     padding: 0.5em 1em;
     height: 40%;
     position: absolute;
-    width:100%;
+    width: 100%;
     bottom: -10%;
     display: flex;
     flex-direction: column;
 
-    background: rgba(40, 40, 40, 0.70);
-    transition: all 0.35s ease-in-out; 
+    background: rgba(40, 40, 40, 0.7);
+    transition: all 0.35s ease-in-out;
 
-    @media screen and (max-width: 414px){
+    @media screen and (max-width: 414px) {
       bottom: 0%;
     }
   }
-  
 
   .card__title {
     font-size: 1.2em;
@@ -126,7 +147,6 @@ export const StyledProductCard = styled.article`
       outline: none;
     }
   } */
-
 `;
 export const DivBtn = styled.div`
   width: 90%;
@@ -137,9 +157,7 @@ export const DivBtn = styled.div`
   bottom: -15px !important;
   margin-bottom: 1rem !important;
   margin-top: 0 !important;
-
-
-`
+`;
 
 export const MiniCard = styled.article`
   position: relative;
@@ -214,41 +232,37 @@ export const MiniCard = styled.article`
   }
 `;
 
-
 export const PriceGenreCont = styled.div`
-display: flex;
-width: 100%;
-align-items: center;
-margin-top: 1rem;
-
-`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  margin-top: 1rem;
+`;
 export const GenresContainer = styled.div`
-width:100%;
-display: flex;
-justify-content: flex-end;
-`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
 export const Genres = styled.h5`
-background: #0a3364;
-width: 40%;
-height:100%;
-margin: 0  0 0 .3rem;
-text-align: center;
-align-items: flex-end;
-padding: .3rem;
-border-radius: 10px;
-
-`
+  background: #0a3364;
+  width: 40%;
+  height: 100%;
+  margin: 0 0 0 0.3rem;
+  text-align: center;
+  align-items: flex-end;
+  padding: 0.3rem;
+  border-radius: 10px;
+`;
 export const Price = styled.h4`
-background: #3fa91e;
-width: 25%;
-height:100%;
-margin-bottom: 0;
-text-align: center;
-border-top-right-radius: 10px;
-border-bottom-right-radius: 10px;
-margin-left: -1rem;
-display:flex;
-align-items: center;
-justify-content: center;
-
-`
+  background: #3fa91e;
+  width: 25%;
+  height: 100%;
+  margin-bottom: 0;
+  text-align: center;
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+  margin-left: -1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
